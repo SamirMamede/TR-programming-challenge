22f778625d0576afc60e1f984b9e817e333d7789
SamirMamede <anuarsamir@gmail.com>
2022-08-23 22:18:28 -0400

    filtering json data

--- app.py
+++ app.py
@@ -3,15 +3,29 @@ import openpyxl
 
 def main() -> None:
 
-    countries = requests.get("https://restcountries.com/v3.1/all")
-    countries = countries.json()
+    url = "https://restcountries.com/v3.1/all"
 
-    planilha = openpyxl.Workbook()
-    planilha.create_chartsheet('Countries')
-    page_countries = planilha['Countries']
+    countries = requests.get(url)
+    data = countries.json()
 
-    page_countries.append(['Name', 'Capital', 'Area', 'Currencies'])
-    page_countries.append([])
+    for i in range (len(data)):
+        countries_name = data[i]['name']['official']
+        countries_capital = data[i]['capital'][0]
+        countries_area = data[i]['area']
+        countries_currencies = data[i]['currencies']
+
+        print(countries_name)
+        print(countries_capital)
+        print(countries_area)
+        print(countries_currencies)
+
+
+#    planilha = openpyxl.Workbook()
+#    planilha.create_chartsheet('Countries')
+#    page_countries = planilha['Countries']
+
+#    page_countries.append(['Name', 'Capital', 'Area', 'Currencies'])
+#    page_countries.append([])
 
 
 if __name__ == "__main__":
